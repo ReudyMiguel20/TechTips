42d70f6e31745387a80cd21fe9bd386e6a75d34d
Reudy Guerrero <reudymiguel20@gmail.com>
2023-10-20 23:44:32 -0400

    First commit
    - Added Github Actions

---
+++ src/main/java/com/techtips/techtips/TechTipsApplication.java
@@ -0,0 +1,13 @@
+package com.techtips.techtips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechTipsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techtips/techtips/TechTipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtips.techtips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTipsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
