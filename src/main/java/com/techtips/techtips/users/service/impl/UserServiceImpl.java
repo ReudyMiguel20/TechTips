48565b97d2901c5704a0b079d3671780f5c1e7c2
Reudy Guerrero <reudymiguel20@gmail.com>
2023-10-21 01:10:27 -0400

    Adding unit tests

--- src/main/java/com/techtips/techtips/users/service/impl/UserServiceImpl.java
+++ src/main/java/com/techtips/techtips/users/service/impl/UserServiceImpl.java
@@ -1,8 +1,15 @@
 package com.techtips.techtips.users.service.impl;
 
+import com.techtips.techtips.users.repository.UserRepository;
 import com.techtips.techtips.users.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+
 }
---
+++ src/test/java/com/techtips/techtips/users/service/impl/UserServiceImplTest.java
@@ -0,0 +1,62 @@
+package com.techtips.techtips.users.service.impl;
+
+import com.techtips.techtips.users.model.dto.RegisterRequest;
+import com.techtips.techtips.users.model.entity.User;
+import com.techtips.techtips.users.repository.UserRepository;
+import com.techtips.techtips.users.service.UserService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class UserServiceImplTest {
+
+    @Autowired
+    private UserServiceImpl userService;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        userService = new UserServiceImpl(userRepository);
+    }
+
+    @Test
+    @DisplayName("Saving User in the Database")
+    void savingUserToDatabase() {
+        // Arrange
+        User newUser = User.builder()
+                .firstName("Mike")
+                .lastName("Carmine")
+                .email("MikeCarmine@gmail.com")
+                .password("123456789")
+                .build();
+
+        when(userRepository.save(newUser)).thenReturn(newUser);
+
+//        Act
+        newUser = userRepository.save(newUser);
+
+
+//        Assert
+        Assertions.assertThat(newUser).isInstanceOf(Math.class);
+    }
+}
